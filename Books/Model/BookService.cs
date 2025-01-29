11f62d6eb2e15574b9756b8b82f37451f529a179
мусатова.анастасия <.@TERM1>
2025-01-29 10:39:23 +0300

    Добавьте файлы проекта.

---
+++ Books/Model/BookService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Model
+{
+    public class BookService
+    {
+        public const string JSON_PATH =
+            "https://mysafeinfo.com/api/data?list=bestnovels&format=json&case=default";
+
+        public List<Book> Books { get; private set; }
+
+        public async Task LoadBookAsync()
+        {
+            try
+            {
+                HttpClient client = new();
+                var response = await client.GetAsync(JSON_PATH);
+            }
+
+            catch
+            {
+
+            }
+        }
+    }
+}
